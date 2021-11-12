580116268919a53a7657733e82dff76cfd916b48
MDA1422 <71685940+Melik-A1422@users.noreply.github.com>
2021-11-12 14:11:22 +0000

    test

---
+++ Tests.py
@@ -0,0 +1,24 @@
+import unittest
+import test_project
+class Tests(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_maths_test(self):
+        q = test_project.Questions()
+        self.assertEqual(q.question(1,2),3)
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_project.py
@@ -0,0 +1,7 @@
+
+class Questions:
+    def question(self, num1,num2):
+        return num1+num2
+    
+    
+    
